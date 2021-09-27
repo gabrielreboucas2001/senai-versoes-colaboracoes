63033111aaf9a0cf0b74f73da2f7e5c035d06d7d
gabrielreboucas2001 <89884709+gabrielreboucas2001@users.noreply.github.com>
2021-09-27 14:52:18 -0300

    Delete Gabriel_Contagem_de_Peças.js

--- Gabriel_Contagem_de_Peças.js
+++
@@ -1,9 +0,0 @@
-let listaDePeças = ["Peça1", "Peça2", "Peça3", "Peça4", "Peça5", "Peça6", "Peça7", "Peça8", "Peça9", "Peça10"];
-let quantidaDePeças = listaDePeças.length;
-if (quantidaDePeças < 10) {
-	listaDePeças.push("Total de peças");
-    console.log(listaDePeças);
-    console.log(quantidaDePeças);
-} else {
-	console.log("Não tem capacidade suficiente para inserir mais peças");
-}
